f6c568f701831d62187d46d21b263d6f74abb41b
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-07-30 10:08:55 +0200

    Fix

--- Js/script.js
+++ Js/script.js
@@ -5,18 +5,16 @@ var km = parseInt(prompt("Inserisci i km da percorrere"));
 // 2. quanti anni hai?
 var age = parseInt(prompt("Inserisci l'età"));
 
+//---------------- CALCOLO PREZZO VIAGGIO ----------------
 var price = (km * 0.21);
 
-//---------------- CALCOLO PREZZO VIAGGIO ----------------
+//---------------- CALCOLO SCONTO ----------------
 // 3. sconto del 20% per i minorenni o 40% per gli over 65
 if (age < 18) {
    var price = km * 0.21 * 80 /100; 
 
 } else if (age > 65) {
     var price = km * 0.21 * 60 /100;
-
-} else {
-    var price = km * 0.21; 
 }
 
 // alert(price);
